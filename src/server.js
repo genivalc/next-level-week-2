76332c48e95c94e8cc09482c72595f9e77147e4f
genivalc <genivalcandeiadon.neto@gmail.com>
2020-08-06 15:10:25 -0300

    Salvando o formulario na cache

--- src/server.js
+++ src/server.js
@@ -32,19 +32,68 @@ const proffys = [
         ]
     },
 ]
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
+function pageStudy(req, res) {
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
+}
+
+function pageLanding(req, res) {
+    return res.render("index.html")
+}
+
+function pageGiveClasses(req, res) {
+    const data = req.query
+    const isNotEmpyt = Object.keys(data).length > 0
+    if (isNotEmpyt) {
+        data.subject = getSubject(data.subject)
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+    
+    return res.render("give-classes.html", { subjects, weekdays })
+}
 
 const express = require('express')
 const server = express()
+const nunjucks = require('nunjucks')
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
 
 server.use(express.static("public"))
-    .get("/", (req, res) => {
-        return res.sendFile(__dirname + "/views/index.html")
-    })
-    .get("/study", (req, res) => {
-        return res.sendFile(__dirname + "/views/study.html")
-    })
-    .get("/give-classes", (req, res) => {
-        return res.sendFile(__dirname + "/views/give-classes.html")
-    })
+    .get("/", pageLanding)
+    .get("/study", pageStudy)
+    .get("/give-classes", pageGiveClasses)
 
     .listen(5000)
